5d64a5766c92942ee5ff3e40cab38902765c04c6
pawelwiecaszek <pawel.wiecaszek@gmail.com>
2019-12-12 19:29:51 +0100

    initial commit

--- generate_mitm_ca.rhel.sh
+++ generate_mitm_ca.rhel.sh
@@ -4,8 +4,8 @@ set -e
 # Generate a MITM certificate and key
 echo '{"CN":"CA","key":{"algo":"rsa","size":2048}}' | cfssl gencert -initca - | cfssljson -bare mitm
 mkdir -p /certificates
-cp mitm-key.pem /certificates/mitm.key
-cp mitm.pem /certificates/mitm.crt
+cp mitm-key.pem /clair/certs/mitm.key
+cp mitm.pem /clair/certs/mitm.crt
 cp mitm.pem /etc/pki/ca-trust/source/anchors/mitm.crt
 
 # This directory is for any custom certificates users want to mount
